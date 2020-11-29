be312017145fa3c74bcb72e318212be0a91caa90
ur-clapped-mate <75200147+ur-clapped-mate@users.noreply.github.com>
2020-11-29 09:10:31 +0000

    Update script.py

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ def application():
     if MsgBox == "yes":  # if user selected yes
         for r, d, f in os.walk("D:\\"):  # what drive it searches
             for files in f:
-                if files == "RainbowSix.exe":  # file it is looking for
+                if files == "test.txt":  # file it is looking for, you also need the file extension e.g. exe or .txt
                     location = os.path.join(r, files)  # finds the location of the file
                     print(location)  # prints the location (mostly for dev purposes)
                     if os.path.exists(location):
